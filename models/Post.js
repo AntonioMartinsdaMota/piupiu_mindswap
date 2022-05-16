cb2ca01e2db95b9cc95e6d19c3604b9e6626cbdf
Antonio Mota <antoniommota@netcabo.pt>
2022-05-16 21:24:25 +0100

    added post and user models

---
+++ models/Post.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const PostSchema = new mongoose.Schema({
+    
+    title: {
+        type: String,
+        required: [true, "Please add a title"],
+        maxlength: [100, "Title can not be more than 100 characters"]
+    },
+    content: {
+        type: String,
+        required: [true, "Please add content"]
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "Please add a user"]
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    comments : [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
+
+});
+
+module.exports = mongoose.models.Post || mongoose.model("Post", PostSchema);
