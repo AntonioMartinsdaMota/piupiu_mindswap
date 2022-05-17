4d330dbf06969a6e3a600f9ba58fe4baa44d36bc
LuisFaria <57047301+luisfcfaria@users.noreply.github.com>
2022-05-17 14:42:44 +0100

    update css

--- components/Header/FeedHeader.js
+++ components/Header/FeedHeader.js
@@ -1,9 +1,13 @@
+import styles from './Header.module.css'
+
 function PageHeader(){
 
     return <>
+    <div className={styles.logo}>
     <img src="https://i.ibb.co/3c94bFF/logo.png" height={75} alt="" />
     <h2>PiuPiu</h2>
     <input className="search_bar" type="text" placeholder="🔍  Search" />
+    </div>
     </>
 }
 
