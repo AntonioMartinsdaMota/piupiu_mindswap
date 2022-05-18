ebcfba18300675986d1b58f0b978fbc2252da396
Antonio Mota <antoniommota@netcabo.pt>
2022-05-18 10:30:56 +0100

    mongo refactor

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     env: {
-        MONGO_URI: "mongodb+srv://piupiu:piupiu@cluster0.rzqmg.mongodb.net/piupiu_db?retryWrites=true&w=majority"
+        MONGO_URI: "mongodb://piupiu:piupiu@cluster0-shard-00-00.rzqmg.mongodb.net:27017,cluster0-shard-00-01.rzqmg.mongodb.net:27017,cluster0-shard-00-02.rzqmg.mongodb.net:27017/piupiu_db?ssl=true&replicaSet=atlas-b61vrt-shard-0&authSource=admin&retryWrites=true&w=majority"
     }
 }
