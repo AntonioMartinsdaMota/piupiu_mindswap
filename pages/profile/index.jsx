2f4b27030956f2af3b6770e124217def050839b5
Antonio Mota <antoniommota@netcabo.pt>
2022-05-19 18:54:44 +0100

    profile page added, avatar and about

---
+++ pages/profile/index.jsx
@@ -0,0 +1,13 @@
+import Avatar from "../../components/profile/Avatar";
+import About from "../../components/profile/About";
+
+function Profile(){
+    return(
+        <div className="Profile">
+            <Avatar/>
+            <About/>
+        </div>
+    );
+}
+
+export default Profile;
