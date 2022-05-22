8562c7d9a76d9a55d78e5cadec52bd3599a0acb1
DiogoBozan <diogo_fb_@hotmail.com>
2022-05-22 23:46:36 +0100

    update css

--- components/profile/Avatar.jsx
+++ components/profile/Avatar.jsx
@@ -1,32 +1,29 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import styles from "./Avatar.module.css";
 
+function Avatar() {
+  const [user, setUser] = useState({});
+  const [avatar, setAvatar] = useState(null);
 
-function Avatar (){
+  async function getUser() {
+    const response = await fetch("api/me");
+    const json = await response.json();
+    const user = json.data;
+    return user;
+  }
 
-    const [user, setUser] = useState({});
-    const [avatar, setAvatar] = useState(null);
+  useEffect(() => {
+    getUser().then((user) => {
+      setUser(user);
+      setAvatar(user.avatar);
+    });
+  }, []);
 
-    async function getUser(){
-        const response = await fetch("api/me");
-        const json = await response.json();
-        const user = json.data;
-       return user
-    }
-
-    useEffect(() => {
-        getUser().then(user => {
-            setUser(user)
-            setAvatar(user.avatar)
-        })
-    }, []);
-
-    return(
-        <div className="avatar">
-            <img className={styles.profileImage} src={avatar} alt="avatar" />
-        </div>
-    );
-
+  return (
+    <div className={styles.avatar}>
+      <img className={styles.profileImage} src={avatar} alt="avatar" />
+    </div>
+  );
 }
 
 export default Avatar;
